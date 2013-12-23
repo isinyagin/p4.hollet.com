f67ab196c761505626e24ae920768391cd8d3785
Ivan Sinyagin <isinyagin@fas.harvard.edu>
2013-12-22 20:40:44 -0500

    Production ready

---
+++ app/config/development/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	'connections' => [
+		'mysql' => [
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'shortUrl',
+			'username'  => 'root',
+			'password'  => 'atikinn',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		]
+	],
+	'migrations' => 'migrations',
+];
+
